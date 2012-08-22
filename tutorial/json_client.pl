b2b32365c4469417abf39d2073ab8bc13cb3bca4
Jeho, Sung <jeho.sung@silex.kr>
2012-08-22 18:06:40 +0900

    getopt 로 받을 수 있도록 처리
    (\t -> 빈칸 으로 변경)

--- tutorial/json_client.pl
+++ tutorial/json_client.pl
@@ -8,48 +8,79 @@ use warnings;
 use Data::Dumper;
 use HTTP::Cookies;
 use JSON::RPC::Legacy::Client;
+use Getopt::Long::Descriptive;
 
 package JSON::RPC::Legacy::ReturnObject; {
-	{
-		no warnings 'redefine';
-		sub new {
-			my ($class, $obj, $json) = @_;
-			my $content = ( $json || JSON->new->utf8 )->decode( $obj->content );
+    {
+        no warnings 'redefine';
+        sub new {
+            my ($class, $obj, $json) = @_;
+            my $content = ( $json || JSON->new->utf8 )->decode( $obj->content );
 
-			my $self = bless {
-				jsontext => $obj->content,
-				content  => $content,
-				obj      => $obj,
-			}, $class;
+            my $self = bless {
+                jsontext => $obj->content,
+                content  => $content,
+                obj      => $obj,
+            }, $class;
 
-			$content->{error} ? $self->is_success(0) : $self->is_success(1);
-			$content->{version} ? $self->version(1.1) : $self->version(0);
+            $content->{error} ? $self->is_success(0) : $self->is_success(1);
+            $content->{version} ? $self->version(1.1) : $self->version(0);
 
-			$self;
-		}
-	}
+            $self;
+        }
+    }
 
-	sub obj {
-		$_[0]->{obj} = $_[1] if defined $_[1];
-		$_[0]->{obj};
-	}
+    sub obj {
+        $_[0]->{obj} = $_[1] if defined $_[1];
+        $_[0]->{obj};
+    }
 
 } 1;
 
 package main;
 
+my ( $opt, $usage ) = describe_options(
+    "$0 <category> <method> [ <params1> ... ]",
+
+      [
+         'user|u=s', "username", 
+        { default => 'jeho.sung@silex.kr' } 
+      ],
+      [ 'passwd|w=s', "password" ],
+      [
+        'uri=s',
+        "the URI to connect to",
+        { default => "http://jehos.silex.kr/bugzilla/jsonrpc.cgi" }
+      ],
+      [ 'class|c=s',  "method class"   ],
+      [ 'method|m=s', "method"         ],
+      [],
+      [ 'verbose|v', "print detail"  ],
+      [ 'help|h',    "print this"    ],
+);
+
+
 my $client = JSON::RPC::Legacy::Client->new;
-my $URI = "http://jehos.silex.kr/bugzilla/jsonrpc.cgi";
+my $URI = $opt->uri;
+my $user = $opt->user;
+my $passwd = $opt->passwd;
+my $method_params = $opt->class . "." . $opt->method;
+
+
+say $method_params;
+
+
+say $usage->text if $opt->help;
 
 my $res = $client->call(
-	$URI,
-	{ # callobj
-		method => 'User.login',
-		params => {
-			login	 => 'jeho.sung@silex.kr',
-			password => 'action+vision',
-		}
-	}
+    $URI,
+    { # callobj
+        method => 'User.login',
+        params => {
+            login     => $user,
+            password => $passwd,
+        }
+    }
 );
 die $client->status_line unless $res;
 die "Error: " . $res->error_message if $res->is_error;
@@ -65,14 +96,14 @@ $client->ua->cookie_jar($cookie_jar);
 # get user info
 #
 $res = $client->call(
-	$URI,
-	{ # callobj
-		method => 'User.get',
-		params => {
-			names => [ 'jeho.sung@silex.kr' ],
-		},
-	},
-	$cookie_jar,
+    $URI,
+    { # callobj
+        method => $method_params,
+        params => {
+            names => [ 'jeho.sung@silex.kr' ],
+        },
+    },
+    $cookie_jar,
 );
 
 die $client->status_line unless $res;

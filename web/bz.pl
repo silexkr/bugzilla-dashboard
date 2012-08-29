3d2b1c5d9e8905a83f0c587f8f7439c89216c583
Hyungsuk Hong <aanoaa@gmail.com>
2012-08-29 23:37:22 +0900

    recent-comments template 작성

--- web/bz.pl
+++ web/bz.pl
@@ -17,6 +17,36 @@ __DATA__
 % title 'Welcome';
 Welcome to the Mojolicious real-time web framework!
 
+@@ recent-comments.html.ep
+% layout 'default';
+% title '최근 변경 이력의 제공';
+<form>
+  <input type="text" placeholder="검색을 시작할 날짜" />
+  <input type="text" placeholder="갯수" />
+  <input type="submit" value="새로고침" />
+</form>
+<table>
+  <thead>
+    <tr>
+      <th>버그 ID</th>
+      <th>제목</th>
+      <th>작성자</th>
+      <th>변경시간</th>
+      <th>댓글요약</th>
+    </tr>
+    <tr>
+      <td>286</td>
+      <td>"최근변경이력"</td>
+      <td>Jeho Sung</td>
+      <td>12:23:00</td>
+      <td>"완료하엿.."</td>
+    </tr>
+  </thead>
+  <tbody>
+  </tbody>
+</table>
+
+
 @@ layouts/default.html.ep
 <!DOCTYPE html>
 <html>
